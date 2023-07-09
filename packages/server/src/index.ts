1766cfe5a2206fd5180c1b8923a2f03728877d81
gedesurya125 <gedesurya125@gmail.com>
2023-07-09 12:43:54 +0800

    update the port of the server, make it listening to the provided port

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,12 +2,14 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { prismaClient } from '../prisma/client.js';
 import { apolloServer } from './apollo/apolloServer.js';
 
+const port = Number(process.env.PORT) || 4000;
+
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(apolloServer, {
-  listen: { port: 4000 },
+  listen: { port },
   context: async ({ req, res }) => ({
     prismaClient,
   }),
