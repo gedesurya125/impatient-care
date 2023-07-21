51a602649230e7a8cb5f8bcd664de61d1e943143
gedesurya125 <gedesurya125@gmail.com>
2023-07-21 23:24:08 +0800

    add remove the branch and scope url to the apollo client server url

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,7 +1,12 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const serverUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`
+const removeBranchAndScopeText = (url: string): string =>
+  url.replace(/(?:-git.*(?=\.vercel))/g, '');
+
+const serverUrl = process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
+  ? `https://${removeBranchAndScopeText(
+      process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
+    )}/api/graphql`
   : process.env.NEXT_PUBLIC_SERVER_URL;
 
 const client = new ApolloClient({
