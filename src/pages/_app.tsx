e87b6b29057fbf5e640a39553eec814c5a97643d
gedesurya125 <gedesurya125@gmail.com>
2023-08-03 16:31:02 +0800

    change the enable the cors of to allow the server fetched from other origin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,10 +21,8 @@ import { config } from 'theme/config';
 import { MainLayout } from 'layouts';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const serverUrl = process?.env?.NEXT_PUBLIC_VERCEL_BRANCH_URL
-    ? `https://${removeBranchAndScopeText(
-        process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
-      )}/api/graphql`
+  const serverUrl = process?.env?.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`
     : process.env.NEXT_PUBLIC_SERVER_URL;
 
   console.log('this is the serverUrl', serverUrl);
