0edc7c7ef1ddc0bec8e71da9ad424db5420d207a
gedesurya125 <gedesurya125@gmail.com>
2023-08-05 07:56:14 +0800

    unarm the function to remove the branch and the scope text

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -4,9 +4,7 @@ export const removeBranchAndScopeText = (url: string): string =>
   url.replace(/(?:-git.*(?=\.vercel))/g, '');
 
 const serverUrl = process?.env?.NEXT_PUBLIC_VERCEL_BRANCH_URL
-  ? `https://${removeBranchAndScopeText(
-      process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
-    )}/api/graphql`
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}/api/graphql`
   : process.env.NEXT_PUBLIC_SERVER_URL;
 
 const client = new ApolloClient({
