6f0cfbb70dcde27a0139e5b9a51e504e14691e49
gedesurya125 <gedesurya125@gmail.com>
2023-08-05 08:26:58 +0800

    try to allow the cors

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -2,6 +2,7 @@ import { startServerAndCreateNextHandler } from '@as-integrations/next';
 
 import { apolloServer } from 'apollo/apolloServer';
 import { prismaClient } from '../../../prisma/client';
+import { allowCors } from 'utils';
 
 const handler = startServerAndCreateNextHandler(apolloServer, {
   context: async (req, res) => ({
@@ -11,5 +12,5 @@ const handler = startServerAndCreateNextHandler(apolloServer, {
   }),
 });
 
-// export default allowCors(handler);
-export default handler;
+export default allowCors(handler);
+// export default handler;
